4d1b38acbbaf2ba479ac7f17275c2f6a5ba0f45a
YCherk <117645287+YCherk@users.noreply.github.com>
2023-12-19 18:08:32 +0000

    Debugging - Enemy turns to face player
    Previous bug with enemy, the player could chase the enemy from behind with no consequence, however, now if the player gets too close the enemy turns around and begins to chase them

--- Research Facility - Haunting Shadows/Assets/Scripts/EnemyAI.cs
+++ Research Facility - Haunting Shadows/Assets/Scripts/EnemyAI.cs
@@ -8,8 +8,10 @@ public class EnemyAI : MonoBehaviour
     public float moveSpeed = 2.0f;
     public Transform player; // Reference to the player
     public float attackDistance = 2.0f; // Distance at which the enemy starts attacking
-    public float sightDistance = 15.0f; // Distance at which the enemy can see the player
-    public float fieldOfView = 60.0f; // Field of view for line of sight
+    public float sightDistance = 15.0f; // Distance at which the enemy can see the player in front
+    public float backSightDistance = 5.0f; // Distance at which the enemy can see the player from behind
+    public float fieldOfView = 60.0f; // Field of view for line of sight in front
+    public float backFieldOfView = 90.0f; // Field of view for line of sight from behind
 
     private float timer;
     private NavMeshAgent agent;
@@ -30,13 +32,20 @@ public class EnemyAI : MonoBehaviour
     {
         float distanceToPlayer = Vector3.Distance(transform.position, player.position);
         bool canSeePlayer = CanSeePlayer(distanceToPlayer);
+        bool canSeePlayerFromBehind = CanSeePlayerFromBehind(distanceToPlayer);
 
-        if (canSeePlayer)
+        if (canSeePlayer || canSeePlayerFromBehind)
         {
             // Chase the player
             isChasingPlayer = true;
             agent.SetDestination(player.position);
             animator.SetBool("IsAttack", distanceToPlayer <= attackDistance);
+
+            // Turn towards the player if detected from behind
+            if (canSeePlayerFromBehind)
+            {
+                TurnTowardsPlayer();
+            }
         }
         else if (isChasingPlayer)
         {
@@ -54,7 +63,7 @@ public class EnemyAI : MonoBehaviour
         animator.SetBool("isIdle", agent.velocity.magnitude < 0.01f);
 
         // Update the enemy's rotation to face the movement direction
-        if (agent.velocity.magnitude > 0.01f)
+        if (agent.velocity.magnitude > 0.01f && !canSeePlayerFromBehind)
         {
             transform.rotation = Quaternion.LookRotation(agent.velocity.normalized);
         }
@@ -77,7 +86,23 @@ public class EnemyAI : MonoBehaviour
         }
         return false;
     }
-
+    bool CanSeePlayerFromBehind(float distanceToPlayer)
+    {
+        if (distanceToPlayer <= backSightDistance)
+        {
+            Vector3 directionToPlayer = (player.position - transform.position).normalized;
+            float angle = Vector3.Angle(-transform.forward, directionToPlayer); // Check angle with the back direction
+            if (angle <= backFieldOfView / 2)
+            {
+                RaycastHit hit;
+                if (Physics.Raycast(transform.position, directionToPlayer, out hit, backSightDistance))
+                {
+                    return hit.transform == player;
+                }
+            }
+        }
+        return false;
+    }
     void Patrol()
     {
         timer += Time.deltaTime;
@@ -100,5 +125,11 @@ public class EnemyAI : MonoBehaviour
         NavMeshHit navHit;
         NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
         return navHit.position;
+    }
+
+    private void TurnTowardsPlayer()
+    {
+        Vector3 directionToPlayer = (player.position - transform.position).normalized;
+        transform.rotation = Quaternion.LookRotation(directionToPlayer);
     }
 }
