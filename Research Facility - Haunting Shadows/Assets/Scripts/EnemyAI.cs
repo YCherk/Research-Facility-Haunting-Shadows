016d91a89d67c8e6dbbbd8cfdc16e0a3ad889b64
YCherk <117645287+YCherk@users.noreply.github.com>
2023-12-23 17:51:30 +0000

    Updated Enemy AI
    Made AI less likely to collide with walls.

--- Research Facility - Haunting Shadows/Assets/Scripts/EnemyAI.cs
+++ Research Facility - Haunting Shadows/Assets/Scripts/EnemyAI.cs
@@ -12,6 +12,7 @@ public class EnemyAI : MonoBehaviour
     public float backSightDistance = 5.0f; // Distance at which the enemy can see the player from behind
     public float fieldOfView = 60.0f; // Field of view for line of sight in front
     public float backFieldOfView = 90.0f; // Field of view for line of sight from behind
+    
 
     private float timer;
     private NavMeshAgent agent;
@@ -36,11 +37,9 @@ public class EnemyAI : MonoBehaviour
 
         if (canSeePlayer || canSeePlayerFromBehind)
         {
-            // Chase the player
             isChasingPlayer = true;
             agent.SetDestination(player.position);
 
-            // Trigger attack animation if within attack distance
             if (distanceToPlayer <= attackDistance)
             {
                 animator.SetTrigger("IsAttack");
@@ -50,7 +49,6 @@ public class EnemyAI : MonoBehaviour
                 animator.SetBool("IsAttack", false);
             }
 
-            // Turn towards the player if detected from behind
             if (canSeePlayerFromBehind)
             {
                 TurnTowardsPlayer();
@@ -58,20 +56,16 @@ public class EnemyAI : MonoBehaviour
         }
         else if (isChasingPlayer)
         {
-            // Stop chasing if player is no longer visible
             isChasingPlayer = false;
             animator.SetBool("IsAttack", false);
         }
         else
         {
-            // Random Patrol
             Patrol();
         }
 
-        // Set isIdle based on whether the enemy is moving or not
         animator.SetBool("isIdle", agent.velocity.magnitude < 0.01f);
 
-        // Update the enemy's rotation to face the movement direction
         if (agent.velocity.magnitude > 0.01f && !canSeePlayerFromBehind)
         {
             transform.rotation = Quaternion.LookRotation(agent.velocity.normalized);
@@ -103,12 +97,13 @@ public class EnemyAI : MonoBehaviour
         }
         return false;
     }
+
     bool CanSeePlayerFromBehind(float distanceToPlayer)
     {
         if (distanceToPlayer <= backSightDistance)
         {
             Vector3 directionToPlayer = (player.position - transform.position).normalized;
-            float angle = Vector3.Angle(-transform.forward, directionToPlayer); // Check angle with the back direction
+            float angle = Vector3.Angle(-transform.forward, directionToPlayer);
             if (angle <= backFieldOfView / 2)
             {
                 RaycastHit hit;
@@ -120,6 +115,7 @@ public class EnemyAI : MonoBehaviour
         }
         return false;
     }
+
     void Patrol()
     {
         timer += Time.deltaTime;
@@ -131,17 +127,23 @@ public class EnemyAI : MonoBehaviour
             timer = 0;
         }
 
-        // Update the walking animation based on velocity
         animator.SetBool("WalkForward", agent.velocity.magnitude > 0.01f);
     }
 
     public static Vector3 RandomNavSphere(Vector3 origin, float dist, int layermask)
     {
         Vector3 randDirection = Random.insideUnitSphere * dist;
-        randDirection += origin;
-        NavMeshHit navHit;
-        NavMesh.SamplePosition(randDirection, out navHit, dist, layermask);
-        return navHit.position;
+
+        for (int i = 0; i < 30; i++)
+        {
+            Vector3 randomPos = origin + randDirection;
+            NavMeshHit navHit;
+            if (NavMesh.SamplePosition(randomPos, out navHit, dist, layermask))
+            {
+                return navHit.position;
+            }
+        }
+        return origin;
     }
 
     private void TurnTowardsPlayer()
